2750b5c1678f656e91301493dd7573402e6d97ad
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-03-08 23:49:48 +0800

    python

--- LunaHook/engines/python/python.cpp
+++ LunaHook/engines/python/python.cpp
@@ -4,33 +4,8 @@
 #include"stackoffset.hpp"
 #include"types.h"
 #include"defs.h"
-const wchar_t* internal_renpy_call_host(const wchar_t* text,int split){
-    return text;
-}
-void hookrenpy(HMODULE module){
-    LoadPyRun(module);
-    patch_fun=patchfontfunction;
-    hook_internal_renpy_call_host();
-}
-void patchfontfunction(){
-    //由于不知道怎么从字体名映射到ttc/ttf文件名，所以暂时写死arial/msyh
-    if(wcslen(embedsharedmem->fontFamily)==0)return;
-    PyRunScript(Renpy_hook_font);
-}
-void hook_internal_renpy_call_host(){
-    HookParam hp_internal;
-    hp_internal.address=(uintptr_t)GetProcAddress(GetModuleHandleW(LUNA_HOOK_DLL),"internal_renpy_call_host");
-    #ifndef _WIN64
-    hp_internal.offset=get_stack(1);
-    hp_internal.split=get_stack(2);
-    #else
-    hp_internal.offset=get_reg(regs::rcx);
-    hp_internal.split=get_reg(regs::rdx);
-    #endif
-    hp_internal.type=USING_SPLIT|USING_STRING|CODEC_UTF16|EMBED_ABLE|EMBED_BEFORE_SIMPLE|EMBED_AFTER_NEW;
-    NewHook(hp_internal, "internal_renpy_call_host");
-    PyRunScript(Renpy_hook_text);
-}
+namespace{
+
 const char* Renpy_hook_font=R"(
 try:
     import os
@@ -61,19 +36,22 @@ except:
 
 const char* Renpy_hook_text=R"(
 def callLunaHost(text,split):
-    import ctypes
     try:
-        internal_renpy_call_host=ctypes.CDLL('LunaHook64').internal_renpy_call_host
-    except:
-        internal_renpy_call_host=ctypes.CDLL('LunaHook32').internal_renpy_call_host
-    internal_renpy_call_host.argstype=ctypes.c_wchar_p,ctypes.c_int
-    internal_renpy_call_host.restype=ctypes.c_wchar_p
-    if isinstance(text,str):
+        import ctypes
         try:
-            _text=text.decode('utf8')
+            internal_renpy_call_host=ctypes.CDLL('LunaHook64').internal_renpy_call_host
         except:
-            _text=text
-        text=internal_renpy_call_host(_text,split)
+            internal_renpy_call_host=ctypes.CDLL('LunaHook32').internal_renpy_call_host
+        internal_renpy_call_host.argstype=ctypes.c_wchar_p,ctypes.c_int
+        internal_renpy_call_host.restype=ctypes.c_wchar_p
+        if isinstance(text,str):
+            try:
+                _text=text.decode('utf8')
+            except:
+                _text=text
+            text=internal_renpy_call_host(_text,split)
+    except:
+        pass
     return text
 try:
     #6.1.0
@@ -142,12 +120,23 @@ except:
     pass
 )";
 
+typedef enum {PyGILState_LOCKED, PyGILState_UNLOCKED}
+    PyGILState_STATE;
+typedef PyGILState_STATE (*PyGILState_Ensure_t)(void);
+typedef void (*PyGILState_Release_t)(PyGILState_STATE);
+typedef int (*PyRun_SimpleString_t)(const char *command);
+
+PyRun_SimpleString_t PyRun_SimpleString;
+PyGILState_Release_t PyGILState_Release;
+PyGILState_Ensure_t PyGILState_Ensure;
+    
 void LoadPyRun(HMODULE module)
 {
     PyGILState_Ensure=(PyGILState_Ensure_t)GetProcAddress(module, "PyGILState_Ensure");
     PyGILState_Release=(PyGILState_Release_t)GetProcAddress(module, "PyGILState_Release");
     PyRun_SimpleString=(PyRun_SimpleString_t)GetProcAddress(module, "PyRun_SimpleString");
 }
+
 void PyRunScript(const char* script)
 {
     if(!(PyGILState_Ensure&&PyGILState_Release&&PyRun_SimpleString))return;
@@ -155,4 +144,34 @@ void PyRunScript(const char* script)
     auto state=PyGILState_Ensure();
     PyRun_SimpleString(script);
     PyGILState_Release(state);
+}
+    
+void hook_internal_renpy_call_host(){
+    HookParam hp_internal;
+    hp_internal.address=(uintptr_t)internal_renpy_call_host;
+    #ifndef _WIN64
+    hp_internal.offset=get_stack(1);
+    hp_internal.split=get_stack(2);
+    #else
+    hp_internal.offset=get_reg(regs::rcx);
+    hp_internal.split=get_reg(regs::rdx);
+    #endif
+    hp_internal.type=USING_SPLIT|USING_STRING|CODEC_UTF16|EMBED_ABLE|EMBED_BEFORE_SIMPLE|EMBED_AFTER_NEW;
+    NewHook(hp_internal, "internal_renpy_call_host");
+    PyRunScript(Renpy_hook_text);
+}
+    
+void patchfontfunction(){
+    //由于不知道怎么从字体名映射到ttc/ttf文件名，所以暂时写死arial/msyh
+    if(wcslen(embedsharedmem->fontFamily)==0)return;
+    PyRunScript(Renpy_hook_font);
+}
+}
+const wchar_t* internal_renpy_call_host(const wchar_t* text,int split){
+    return text;
+}
+void hookrenpy(HMODULE module){
+    LoadPyRun(module);
+    patch_fun=patchfontfunction;
+    hook_internal_renpy_call_host();
 }
