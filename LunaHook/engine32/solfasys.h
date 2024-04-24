779691b61a8baf5d15e77a85353934f206f7258f
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-25 00:01:45 +0800

    issues/19

--- LunaHook/engine32/solfasys.h
+++ LunaHook/engine32/solfasys.h
@@ -25,12 +25,41 @@
   }
 }
 */
+
+//https://vndb.org/v5175
+//ななちゃんといっしょ
+
+/*
+{
+  BLOCK "StringFileInfo"
+  {
+    BLOCK "041104b0"
+    {
+      VALUE "Comments",          "Solfa Standard Novel System(Based on STUDIO SELDOM Adventure System Version 5.20/(c)AKIYAMA Kouhei)"
+      VALUE "CompanyName",       "sol-fa-soft"
+      VALUE "FileDescription",   "solfasys"
+      VALUE "FileVersion",       "1, 0, 0, 0"
+      VALUE "InternalName",      "solfasys"
+      VALUE "LegalCopyright",    "sol-fa-soft"
+      VALUE "OriginalFilename",  "solfasys.exe"
+      VALUE "ProductName",       "solfasys"
+      VALUE "ProductVersion",    "1, 0, 0, 0"
+    }
+  }
+  BLOCK "VarFileInfo"
+  {
+    VALUE "Translation", 0x411, 1200
+  }
+}
+*/
 class solfasys:public ENGINE{
     public:
     solfasys(){
         
-        check_by=CHECK_BY::RESOURCE_STR;
-        check_by_target=L"Solfa Novel System"; 
+        check_by=CHECK_BY::CUSTOM;
+        check_by_target=[](){
+          return Util::SearchResourceString(L"Solfa Novel System")||Util::SearchResourceString(L"Solfa Standard Novel System");
+        }; 
         is_engine_certain=false;
     };
      bool attach_function();
