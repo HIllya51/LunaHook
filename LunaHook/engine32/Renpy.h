d137d0343c16c183780fce9603caa990ce28cc12
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-27 00:02:44 +0800

    issues/690

--- LunaHook/engine32/Renpy.h
+++ LunaHook/engine32/Renpy.h
@@ -3,12 +3,13 @@
 class Renpy:public ENGINE{
     public:
     Renpy(){
-        
-        check_by=CHECK_BY::CUSTOM; 
-        check_by_target=[](){
-            //Renpy - sample game https://vndb.org/v19843
-            return Util::CheckFile(L"*.py")|| GetModuleHandleW(L"librenpython.dll");
-        };
+        //使用lunatranslator启动游戏，会把cwd修改成exe所在目录，其中没有.py
+        check_by=CHECK_BY::ALL_TRUE;
+        // check_by=CHECK_BY::CUSTOM; 
+        // check_by_target=[](){
+        //     //Renpy - sample game https://vndb.org/v19843
+        //     return Util::CheckFile(L"*.py")|| GetModuleHandleW(L"librenpython.dll");
+        // };
     }; 
     bool attach_function(); 
 };
