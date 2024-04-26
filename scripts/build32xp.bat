49b586f62ea332fa5fca8400b3604390f578b162
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-26 18:31:20 +0800

    fix

--- scripts/build32xp.bat
+++ scripts/build32xp.bat
@@ -1,7 +1,7 @@
-cmake ../CMakeLists.txt -G "Visual Studio 16 2019" -A win32 -T v141_xp -B ../build/x86_xp
+cmake -DWINXP=1 ../CMakeLists.txt -G "Visual Studio 16 2019" -A win32 -T v141_xp -B ../build/x86_xp
 cmake --build ../build/x86_xp --config Release --target ALL_BUILD -j 14
 
-cmake -DLANGUAGE=Chinese ../CMakeLists.txt -G "Visual Studio 16 2019" -A win32 -T v141_xp -B ../build/x86_zh_xp
+cmake -DWINXP=1 -DLANGUAGE=Chinese ../CMakeLists.txt -G "Visual Studio 16 2019" -A win32 -T v141_xp -B ../build/x86_zh_xp
 cmake --build ../build/x86_zh_xp --config Release --target ALL_BUILD -j 14
 python edit_target.py
 
