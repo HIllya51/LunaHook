1645418029f43e27c722fb8647613b53562e60ab
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-04-26 12:20:10 +0800

    remove

--- build.py
+++ build.py
@@ -14,11 +14,12 @@ def installVCLTL():
     subprocess.run(f"7z x {vcltlFileName} -oVC-LTL5")
     os.chdir("VC-LTL5")
     subprocess.run("cmd /c Install.cmd")
-installVCLTL()
 os.chdir(os.path.join(rootDir,'scripts'))
 if int(sys.argv[1])==32:
+    installVCLTL()
     os.system('cmd /c build32.bat')
 elif int(sys.argv[1])==64:
+    installVCLTL()
     os.system('cmd /c build64.bat')
 else:
     os.system('cmd /c pack.bat')
