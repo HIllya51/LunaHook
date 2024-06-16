22be914611fa3b5a76bf3c117a622c4077400fa7
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-06-16 15:49:23 +0800

    nexas

--- LunaHook/engine32/NeXAS.h
+++ LunaHook/engine32/NeXAS.h
@@ -4,12 +4,11 @@ class NeXAS:public ENGINE{
     public:
     NeXAS(){
         
-        check_by=CHECK_BY::FILE_ALL;
-        check_by_target=check_by_list{L"*.pac",L"Thumbnail.pac"};
-        // jichi 6/3/2014: AMUSE CRAFT and SOFTPAL
-    // Selectively insert, so that lstrlenA can still get correct text if failed
-    //if (Util::CheckFile(L"dll\\resource.dll") && Util::CheckFile(L"dll\\pal.dll") && InsertAmuseCraftHook())
-    //  return true;
+        is_engine_certain=false;
+        check_by=CHECK_BY::CUSTOM;
+        check_by_target=[](){
+            return (Util::CheckFile(L"*.pac")&&(Util::CheckFile(L"Thumbnail.pac")||Util::CheckFile(L"Thumbnail5.pac")));
+        };
     }; 
      bool attach_function(); 
 };
