d93ecc95a211c317c4e90e91f4f0d43ae7d7718e
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-06-22 23:00:29 +0800

    build

--- Lang/Lang.h
+++ Lang/Lang.h
@@ -3,11 +3,15 @@
 
 #define English 0
 #define Chinese 1
+#define Russian 2
 
 #include"en.h"
 
 #if (LANGUAGE == Chinese)
 #include"zh.h"
+#endif
+#if (LANGUAGE == Russian)
+#include"ru.h"
 #endif
 
 
--- build.py
+++ build.py
@@ -16,7 +16,25 @@ def installVCLTL():
     subprocess.run(f"7z x temp\\{vcltlFileName} -otemp\\VC-LTL5")
     subprocess.run("cmd /c temp\\VC-LTL5\\Install.cmd")
 
+def build_langx(lang):
+    os.chdir('./scripts')
+    os.system(rf'''
+cmake -DBUILD_PLUGIN=OFF -DLANGUAGE={lang} ../CMakeLists.txt -G "Visual Studio 17 2022" -A win32 -T host=x86 -B ../build/x86_{lang}
+cmake --build ../build/x86_{lang} --config Release --target ALL_BUILD -j 14
 
+cmake -DBUILD_PLUGIN=OFF -DLANGUAGE={lang} ../CMakeLists.txt -G "Visual Studio 17 2022" -A x64 -T host=x64 -B ../build/x64_{lang}
+cmake --build ../build/x64_{lang} --config Release --target ALL_BUILD -j 14
+
+''')
+    
+def build_langx_xp(lang):
+    os.chdir('./scripts')
+    os.system(rf'''
+
+cmake -DBUILD_PLUGIN=OFF -DWINXP=1 -DLANGUAGE={lang} ../CMakeLists.txt -G "Visual Studio 16 2019" -A win32 -T v141_xp -B ../build/x86_{lang}_xp
+cmake --build ../build/x86_{lang}_xp --config Release --target ALL_BUILD -j 14
+call dobuildxp.bat
+''')
     
 if sys.argv[1]=='pack':
     os.chdir(os.path.join(rootDir, "scripts"))
@@ -28,11 +46,15 @@ else:
         os.system(f"cmd /c buildplugin32.bat")
     elif sys.argv[1]=='plg64':
         os.system(f"cmd /c buildplugin64.bat")
-    elif sys.argv[1]=='en':
-        os.system(f"cmd /c builden.bat")
-    elif sys.argv[1]=='zh':
-        os.system(f"cmd /c buildzh.bat")
+    elif sys.argv[1]=='Release_English':
+        build_langx('English')
+    elif sys.argv[1]=='Release_Chinese':
+        build_langx('Chinese')
+    elif sys.argv[1]=='Release_Russian':
+        build_langx('Russian')
     elif sys.argv[1]=='Release_English_winxp':
-        os.system(f"cmd /c buildxpen.bat")
+        build_langx_xp('English')
     elif sys.argv[1]=='Release_Chinese_winxp':
-        os.system(f"cmd /c buildxpzh.bat")
+        build_langx_xp('Chinese')
+    elif sys.argv[1]=='Release_Russian_winxp':
+        build_langx_xp('Russian')
