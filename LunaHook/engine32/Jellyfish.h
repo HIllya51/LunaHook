71683851b682597fa57c28ff6bcf712dddbf5f17
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-07-28 01:34:40 +0800

    dv

--- LunaHook/engine32/Jellyfish.h
+++ LunaHook/engine32/Jellyfish.h
@@ -1,13 +1,24 @@
 
 
-class Jellyfish:public ENGINE{
-    public:
-    Jellyfish(){
-        
-        is_engine_certain=false;
-        check_by=CHECK_BY::FILE_ALL;
-        check_by_target=check_by_list{L"ism.dll"};//,L"data.isa"};
+class Jellyfish : public ENGINE
+{
+public:
+    HMODULE ism;
+    DWORD minaddr, maxaddr;
+    Jellyfish()
+    {
+
+        is_engine_certain = false;
+        check_by = CHECK_BY::CUSTOM;
+        check_by_target = [this]()
+        {
+            ism = GetModuleHandle(L"ism.dll");
+            return ism;
+        };
+        // check_by_list{L"ism.dll"};//,L"data.isa"};
     };
-     bool attach_function();
+    bool attach_function();
+    bool Jellyfish_attach_function();
+    bool Jellyfish_attach_function2();
+    bool Jellyfish_attach_function3();
 };
- 
