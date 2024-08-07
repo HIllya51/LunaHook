e864f62aef67f81be05a6dafa9cb582eed81e344
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-08-07 22:02:36 +0800

    host

--- LunaHost/GUI/LunaHost.cpp
+++ LunaHost/GUI/LunaHost.cpp
@@ -355,7 +355,7 @@ LunaHost::LunaHost()
                     {
             if (HttpRequest httpRequest{
                 L"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-                L"lunatranslator.xyz",
+                L"lunatranslator.org",
                 L"GET",
                 L"/version_lunahook"
             }){
