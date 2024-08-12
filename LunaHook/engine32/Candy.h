d31698c5d1b5a5d52f664c232d968e5ea33357f4
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-08-12 16:46:39 +0800

    willow

--- LunaHook/engine32/Candy.h
+++ LunaHook/engine32/Candy.h
@@ -1,24 +1,27 @@
 
 
-class Candy:public ENGINE{
-    public:
-    Candy(){
-        
-        check_by=CHECK_BY::FILE_ANY;
-        check_by_target=check_by_list{L"*.fpk",L"data\\*.fpk"};
-        is_engine_certain=false;
+class Candy : public ENGINE
+{
+public:
+    Candy()
+    {
+
+        check_by = CHECK_BY::FILE_ANY;
+        check_by_target = check_by_list{L"*.fpk", L"data\\*.fpk"};
+        is_engine_certain = false;
     };
-     bool attach_function();
+    bool attach_function();
 };
 
+class WillowSoft : public ENGINE
+{
+public:
+    WillowSoft()
+    {
 
-class WillowSoft:public ENGINE{
-    public:
-    WillowSoft(){
-        
-        check_by=CHECK_BY::FILE;
-        check_by_target=L"Selene.dll";
-        is_engine_certain=false;
+        check_by = CHECK_BY::FILE;
+        check_by_target = L"Selene.dll";
+        is_engine_certain = false;
     };
-     bool attach_function();
+    bool attach_function();
 };
