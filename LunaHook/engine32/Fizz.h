4d68e318d89ce106026b7d5c0e64cfdddc4f0a4e
恍兮惚兮 <101191390+HIllya51@users.noreply.github.com>
2024-08-13 15:40:26 +0800

    GSD

--- LunaHook/engine32/Fizz.h
+++ LunaHook/engine32/Fizz.h
@@ -1,11 +1,27 @@
 
 
-class Fizz:public ENGINE{
-    public:
-    Fizz(){
-        
-        check_by=CHECK_BY::FILE_ALL;
-        check_by_target=check_by_list{L"data.gsp",L"Image*.gsp",L"se.gsp",L"bgm*.gsp",L"voice/*.gsp"};
+class Fizz : public ENGINE
+{
+public:
+    Fizz()
+    {
+        check_by = CHECK_BY::CUSTOM;
+        check_by_target = [&]()
+        {
+            auto _ = Util::CheckFile(L"data.gsp") && Util::CheckFile(L"Image*.gsp") && Util::CheckFile(L"bgm*.gsp") && Util::CheckFile(L"se.gsp");
+            if (Util::CheckFile(L"voice/*.gsp"))
+            {
+                typex = 1;
+                return true;
+            }
+            if (wcscmp(processName_lower, L"gsd.exe") == 0)
+            {
+                typex = 2;
+                return true;
+            }
+            return false;
+        };
     };
-     bool attach_function();
+    bool attach_function();
+    int typex = 0;
 };
