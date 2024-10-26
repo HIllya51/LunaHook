4a56a39d68be9bc84c39cb86900cf603ebec09ca
test123456654321 <16307130148@fudan.edu.cn>
2024-10-26 11:58:19 +0800

    muv

--- LunaHook/engine32/RUGP.h
+++ LunaHook/engine32/RUGP.h
@@ -1,14 +1,16 @@
 
 
-class RUGP:public ENGINE{
-    public:
-    RUGP(){
-        
-        check_by=CHECK_BY::CUSTOM;
-        check_by_target=[](){
-            return (wcsstr(processName_lower,L"rugp") || Util::CheckFile(L"rugp.exe"));
-             
+class RUGP : public ENGINE
+{
+public:
+    RUGP()
+    {
+
+        check_by = CHECK_BY::CUSTOM;
+        check_by_target = []()
+        {
+            return (wcsstr(processName_lower, L"rugp") || Util::CheckFile(L"rugp.exe"));
         };
     };
-     bool attach_function();
+    bool attach_function();
 };
