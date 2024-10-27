bf0f8877b524036699f29a2d40e5547466d2254f
test123456654321 <16307130148@fudan.edu.cn>
2024-10-27 23:51:02 +0800

    fix

--- include/stringutils.cpp
+++ include/stringutils.cpp
@@ -51,7 +51,7 @@ inline std::vector<StringT> strSplit_impl(const StringT &s, const StringT &delim
 template <class StringT>
 inline bool endWith_impl(const StringT &s, const StringT &s2)
 {
-  if (s2.size() && (s.size() >= s2.size()) && (s.substr(s.size() - s2.size(), s2.size()) == s2))
+  if ((s.size() >= s2.size()) && (s.substr(s.size() - s2.size(), s2.size()) == s2))
   {
     return true;
   }
@@ -61,7 +61,7 @@ inline bool endWith_impl(const StringT &s, const StringT &s2)
 template <class StringT>
 inline bool startWith_impl(const StringT &s, const StringT &s2)
 {
-  if (s2.size() && (s.size() >= s2.size()) && (s.substr(0, s2.size()) == s2))
+  if ((s.size() >= s2.size()) && (s.substr(0, s2.size()) == s2))
   {
     return true;
   }
